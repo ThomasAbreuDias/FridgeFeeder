1f05d77fa3741aba751171ea42a9638624140936
Thomas Abreu Dias <tabreudias@mamasam.net>
2020-05-21 16:42:40 +0200

    Ajout de plusieurs listes

--- App.vue
+++ App.vue
@@ -1,17 +1,73 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
+  	<Statusbar color="#DECDF5"/>
+  	<Header title="Fridge Feeder"/>
+  	<Liste v-for="liste in listes":key="liste.id" :title="liste.title"/>
+
+  	<text-input class="input" v-model="newListText"/>
+  	<touchable-opacity class ="add_btn" :on-press="newList">
+  		<text class="btn_txt">Nouvelle liste</text>
+  	</touchable-opacity>
   </view>
 </template>
 
+<script>
+	import Statusbar from './components/Statusbar';
+	import Header from './components/Header';
+	import Liste from './components/Liste';
+export default {
+	data () {
+		return {
+			newListText: '',
+			listes: [
+				{
+					id: 0,
+					title: 'Liste pour le 15/05/2020',
+				},
+				{
+					id: 1,
+					title: 'Liste pour le 25/07/2020',
+				}
+			]
+		};
+	},
+	components: {
+	    Statusbar, 
+	    Header,
+	    Liste
+	},
+	methods: {
+		newList() {
+			this.listes.push({
+				id: this.listes.length+1,
+				title: this.newListText,
+			});
+			this.newListText = '';
+		}
+	}
+};
+</script>
+
 <style>
-.container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.text-color-primary {
-  color: blue;
-}
+	.container {
+	 	background-color: white;
+	 	flex: 1;
+	}
+	.add_btn {
+		width: 150px;
+		height: 35px;
+		justify-content: center;
+		align-items: center;
+		background-color: #DECDF5;
+	}
+	.btn_txt {
+		color: #534D56;
+		font-size: 18px;
+		font-weight: 700;
+	}
+	.input {
+		background-color: #F8F1FF;
+		height: 30px;
+		font-size: 18px;
+	}
 </style>
---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  	<view class="header">
+  		<text class="title">{{ title }}</text>
+  	</view>
+</template>
+
+<script>
+	export default {
+	  props: ['title']
+	}
+</script>
+
+<style>
+	.header {
+		height: 62px; 
+		background-color: #534D56;
+		justify-content: center;
+		align-items: center;
+	}
+	.title {
+		color: #F8F1FF;
+		font-size: 30px;
+		font-weight: 900;
+	}
+</style>
